f0e88c1baa9b4639115b90888202b330ef834a5d
przemyslawren <przemyslaw.ren@gmail.com>
2025-08-14 13:30:43 +0200

    add db_config.sql script

---
+++ db_config.sql
@@ -0,0 +1,32 @@
+-- DB Version: 17
+-- OS Type: linux
+-- DB Type: web
+-- Total Memory (RAM): 2
+-- Data Storage: ssd
+
+ALTER SYSTEM SET
+    max_connections = '200';
+ALTER SYSTEM SET
+    shared_buffers = '512MB';
+ALTER SYSTEM SET
+    effective_cache_size = '1536MB';
+ALTER SYSTEM SET
+    maintenance_work_mem = '128MB';
+ALTER SYSTEM SET
+    checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET
+    wal_buffers = '16MB';
+ALTER SYSTEM SET
+    default_statistics_target = '100';
+ALTER SYSTEM SET
+    random_page_cost = '1.1';
+ALTER SYSTEM SET
+    effective_io_concurrency = '200';
+ALTER SYSTEM SET
+    work_mem = '2520kB';
+ALTER SYSTEM SET
+    huge_pages = 'off';
+ALTER SYSTEM SET
+    min_wal_size = '1GB';
+ALTER SYSTEM SET
+    max_wal_size = '4GB';
