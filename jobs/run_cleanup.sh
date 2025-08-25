89e6b01413c0e6e8bbf54ceec98fa0adc0bd0587
przemyslawren <przemyslaw.ren@gmail.com>
2025-08-25 21:32:27 +0200

    update paths

--- jobs/run_cleanup.sh
+++ jobs/run_cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "$(date +%d-%m-%Y)" >> /home/reno/Scripts/cleanup.log
-echo "Archived tasks job cleanup started..." >> /home/reno/Scripts/cleanup.log
+echo "$(date +%d-%m-%Y)" >> /home/ec2-user/pg_jobs/cleanup.log
+echo "Archived tasks job cleanup started..." >> /home/ec2-user/pg_jobs/cleanup.log
 
 /usr/bin/docker exec -i listtick-db \
   psql "dbname=task user=postgres" -v ON_ERROR_STOP=1 \
-  < /home/reno/Scripts/db_cleanup_job.sql \
-  >> /home/reno/Scripts/cleanup.log 2>&1
+  < /home/ec2-user/pg_jobs/db_cleanup_job.sql \
+  >> /home/ec2-user/pg_jobs/cleanup.log 2>&1
 
-echo "Archived tasks job cleanup finished." >> /home/reno/Scripts/cleanup.log
+echo "Archived tasks job cleanup finished." >> /home/ec2-user/pg_jobs/cleanup.log
